b2577852816f045d34442f65b6fd1ef6d7bd9616
Rebecca Romeo <rebecca.romeo@ryerson.ca>
2023-02-21 01:07:24 -0500

    created set and fetch requests, tested out variable accessibility between requests

--- express_server.js
+++ express_server.js
@@ -22,6 +22,19 @@ app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
 
+// ---------------------------------------------
+// Test: variable accessibility between requests
+
+// app.get("/set", (req, res) => {
+//   const a = 1;
+//   res.send(`a = ${a}`);
+// });
+
+// app.get("/fetch", (req, res) => {
+//   res.send(`a = ${a}`);
+// });
+// END test --------------------------------------
+
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
