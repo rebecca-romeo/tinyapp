d98020faecdf7aa50558c8419b088e6a0724ba9b
Rebecca Romeo <rebecca.romeo@ryerson.ca>
2023-02-22 17:43:33 -0500

    added form html/css and created get and post methods to handle the data

--- express_server.js
+++ express_server.js
@@ -3,15 +3,35 @@ const app = express();
 const PORT = 3001; // default port 8080
 
 app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: true }));
 
+// Database object
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
 
+
+
+
+
+
+
 // Route: Homepage - Displays Hello string
 app.get("/", (req, res) => {
   res.send("Hello!");
+});
+
+
+
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
+// Route: urls/id
+app.get("/urls/:id", (req, res) => {
+  const templateVars = { id: req.params.id, longURL: req.params.longURL /*  What goes here? */ };
+  res.render("urls_show", templateVars);
 });
 
 // Route: urls
@@ -20,10 +40,10 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 });
 
-// Route: urls/id
-app.get("/urls/:id", (req, res) => {
-  const templateVars = { id: req.params.id, longURL: req.params.longURL /*  What goes here? */ };
-  res.render("urls_show", templateVars);
+// Route: post urls
+app.post("/urls", (req, res) => {
+  console.log(req.body); // Log the POST request body to the console
+  res.send("Ok"); // Respond with 'Ok' (we will replace this)
 });
 
 // Route: urls.json - Displays urlDatabase as json string
