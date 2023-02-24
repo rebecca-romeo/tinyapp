44b564e1ceb89ad6282c0e6c14b300e22d8d878e
Rebecca Romeo <rebecca.romeo@ryerson.ca>
2023-02-23 20:12:12 -0500

    added post/register route that captures users id, email, password into the users object

--- express_server.js
+++ express_server.js
@@ -9,6 +9,10 @@ const PORT = 3001; // default port 8080
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
 
+const users = {
+
+};
+
 // Database object
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -35,37 +39,65 @@ app.get("/", (req, res) => {
   res.send("Hello!");
 });
 
+// Route: register
+app.get('/register', (req, res) => {
+  res.render('register');
+});
+
+// Route: register
+app.post("/register", (req, res) => {
+  // create a random id
+  const userId = generateRandomString();
+  // use random id to create a new user
+  const newUser = `user${userId}`;
+  // add the new user & info to the global users object
+  users[newUser] = {
+    id: userId,
+    email: req.body.email,
+    password: req.body.password
+  }
+
+    console.log(users);
+  // create a cookie with the user's id
+  res.cookie('user_id', userId);
+
+
+  res.redirect('/urls');
+});
+
+
+
 // Route: login
 // user types a username in form (_header.ejs), hits submit.
 // A cookie is create with their login name and value. Once logged in, user is redirected to /urls
 app.post("/login", (req, res) => {
   const username = req.body.username;
-  res.cookie('username', username)
-  res.redirect('/urls')
+  res.cookie('username', username);
+  res.redirect('/urls');
 });
 
 
 // Route: logout
 app.post("/logout", (req, res) => {
   res.clearCookie('username');
-  res.redirect('/urls')
+  res.redirect('/urls');
 });
 
 
 // Route: submit url
-  // a form (from urls_new ejs)
-  // use types a website and hits submit
+// a form (from urls_new ejs)
+// use types a website and hits submit
 app.get("/urls/new", (req, res) => {
   const templateVars = {
     username: req.cookies['username']
-  }
+  };
   res.render("urls_new", templateVars);
 });
 
 // Route: post submitted url
-  // after user submits the website, a short url is generated
-  // user is then redirected to urls/tinyurl to see their newly generate tiny url and corresponding long url
-  // (uses longURL / submit button from urls_new ejs)
+// after user submits the website, a short url is generated
+// user is then redirected to urls/tinyurl to see their newly generate tiny url and corresponding long url
+// (uses longURL / submit button from urls_new ejs)
 app.post("/urls", (req, res) => {
   // On post, generate a random string
   const randomString = generateRandomString();
@@ -73,12 +105,13 @@ app.post("/urls", (req, res) => {
   // Add the random string to the database as a key, and the value is the longurl that the user submitted
   urlDatabase[randomString] = req.body.longURL;
 
+
   // Redirect the user to the urls/:id page
   res.redirect(`/urls/${randomString}`);
 });
 
 // Route: short url id redirects to long url website
-  // use a tiny url id and brings user to the real website
+// use a tiny url id and brings user to the real website
 app.get("/u/:id", (req, res) => {
   const longURL = urlDatabase[req.params.id];
   res.redirect(longURL);
@@ -86,9 +119,9 @@ app.get("/u/:id", (req, res) => {
 
 
 // Route: urls/id
-  // page after the user creates a tiny url
-  // user can view the short and long url
-  // user can make an edit to the long url
+// page after the user creates a tiny url
+// user can view the short and long url
+// user can make an edit to the long url
 app.get("/urls/:id", (req, res) => {
   const templateVars = {
     username: req.cookies["username"],
@@ -99,14 +132,14 @@ app.get("/urls/:id", (req, res) => {
 });
 
 // Route: delete row
-  //user clicks delete button, deletes the entire line of short and long url that was saved (button in urls_index)
+//user clicks delete button, deletes the entire line of short and long url that was saved (button in urls_index)
 app.post("/urls/:id/delete", (req, res) => {
   delete urlDatabase[req.params.id];
   res.redirect('/urls');
 });
 
 // Route: edit longurl
-  //takes the longurl the user submit and replaces the original url (form in urls_show ejs)
+//takes the longurl the user submit and replaces the original url (form in urls_show ejs)
 app.post("/urls/:id", (req, res) => {
   const shortID = req.params.id;
   const longURL = req.body.editURL;
@@ -135,11 +168,6 @@ app.get("/urls", (req, res) => {
 //   res.json(urlDatabase);
 // });
 
-
-// Route: hello - Displays Hello World using html
-// app.get("/hello", (req, res) => {
-//   res.send("<html><body>Hello <b>World</b></body></html>\n");
-// });
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
