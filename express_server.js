176cf88596b11e5e314803cb1ba0efe94a0426f2
Rebecca Romeo <rebecca.romeo@ryerson.ca>
2023-04-18 17:45:44 -0400

    added delete route and form/delete buttons to each table item (each url)

--- express_server.js
+++ express_server.js
@@ -67,6 +67,11 @@ app.post("/urls", (req, res) => {
   res.redirect(`/urls/${shortUrl}`);
 });
 
+app.post("/urls/:id/delete", (req, res) => {
+  delete urlDatabase[req.params.id];
+  res.redirect('/urls')
+});
+
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
