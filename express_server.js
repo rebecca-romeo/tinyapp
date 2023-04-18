be51099ebdd2ab03c20daf23df9e344c7c3b2b4c
Rebecca Romeo <rebecca.romeo@ryerson.ca>
2023-04-18 04:59:11 -0400

    created urls_index ejs, added /urls route and passed template vars

--- express_server.js
+++ express_server.js
@@ -13,12 +13,10 @@ app.get("/", (req, res) => {
   res.send("Hello!");
 });
 
-app.get("/urls.json", (req, res) => {
-  res.json(urlDatabase);
-});
-
-app.get("/hello", (req, res) => {
-  res.send("<html><body>Hello <b>World</b></body></html>\n");
+app.get("/urls", (req, res) => {
+  const templateVars = { urls: urlDatabase };
+  console.log("temp vars:", templateVars)
+  res.render("urls_index", templateVars);
 });
 
 app.listen(PORT, () => {
