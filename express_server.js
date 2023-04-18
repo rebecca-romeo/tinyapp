c2d4f1e58f66f02c5c9ac323a748d55c47a18cb4
Rebecca Romeo <rebecca.romeo@ryerson.ca>
2023-04-18 06:02:43 -0400

    created urls_new ejs and  /urls/new to display the form

--- express_server.js
+++ express_server.js
@@ -19,6 +19,10 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 });
 
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
 app.get("/urls/:id", (req, res) => {
   const templateVars = {
     id: req.params.id,
