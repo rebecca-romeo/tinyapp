a159348019be77b27024e32a1371e6274f1e0074
Rebecca Romeo <rebecca.romeo@ryerson.ca>
2023-04-18 20:26:56 -0400

    Add POST route for /urls/:id to update a resource

--- express_server.js
+++ express_server.js
@@ -72,6 +72,12 @@ app.post("/urls/:id/delete", (req, res) => {
   res.redirect('/urls')
 });
 
+app.post('/urls/:id', (req, res) => {
+  const id = req.params.id;
+  urlDatabase[id] = req.body.editURL;
+  res.redirect(`/urls/${id}`)
+})
+
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
