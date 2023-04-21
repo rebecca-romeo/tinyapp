495cc483266d20658f22c47c9476fb2e77bf09f0
Rebecca Romeo <rebecca.romeo@ryerson.ca>
2023-04-21 05:11:54 -0400

    Eslint

--- express_server.js
+++ express_server.js
@@ -30,14 +30,15 @@ const users = {
   userRandomID: {
     id: "userRandomID",
     email: "user@example.com",
-    password: "purple-monkey-dinosaur",
+    password: "$2a$10$u7cqKkuIqOjenKJCX5XSHOLMyQRArqCXk.Zkt1Bti4ht13e2p97wG"
   },
   user2RandomID: {
     id: "user2RandomID",
     email: "user2@example.com",
-    password: "dishwasher-funk",
+    password: "$2a$10$oNYgh7N8X92224imwO5iAOaz776q3FtjPgaq.9s6/xp23voNxPwNC"
   },
 };
+
 
 // ---- GET ROUTES -----
 // Homepage
