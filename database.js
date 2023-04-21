fd6dc37a49f5ba56b14d782654a5d2025cdd7007
Rebecca Romeo <rebecca.romeo@ryerson.ca>
2023-04-21 05:50:48 -0400

    update to show longURL after db structure change

---
+++ database.js
@@ -0,0 +1,30 @@
+// ---- DATABASES -----
+
+const users = {
+  userRandomID: {
+    id: "userRandomID",
+    email: "user@example.com",
+    password: "$2a$10$u7cqKkuIqOjenKJCX5XSHOLMyQRArqCXk.Zkt1Bti4ht13e2p97wG"
+  },
+  user2RandomID: {
+    id: "user2RandomID",
+    email: "user2@example.com",
+    password: "$2a$10$oNYgh7N8X92224imwO5iAOaz776q3FtjPgaq.9s6/xp23voNxPwNC"
+  },
+};
+
+const urlDatabase = {
+  b6UTxQ: {
+    longURL: "https://www.tsn.ca",
+    userID: "aJ48lW",
+  },
+  i3BoGr: {
+    longURL: "https://www.google.ca",
+    userID: "aJ48lW",
+  },
+};
+
+module.exports = {
+  users,
+  urlDatabase
+}
